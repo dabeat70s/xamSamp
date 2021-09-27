a81a7d0d225e5e1e5455c031a6b7f8525656ae22
dabeat <dabeat1@hotmail.com>
2021-09-27 12:17:44 +0100

    initComm Files

---
+++ MobileApp.XAML/MobileApp.XAML/ReferencePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MobileApp.XAML
+{
+    public partial class ReferencePage : ContentPage
+    {
+        public ReferencePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
